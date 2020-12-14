83d475591099572c323f32c706862aad9ab92325
Moisez Dantas <moisez.dantas@visto.global>
2020-12-14 17:56:48 -0300

    implemntation apllication.properties, apllication-test.properties, project

---
+++ backend/src/test/java/br/com/devesuperior/movieflix/MovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devesuperior.movieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
