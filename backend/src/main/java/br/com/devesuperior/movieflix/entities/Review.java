1aec24d4c89bcf3b123f42191e8787fa0e3bf50d
Moisez Dantas <moisez.dantas@visto.global>
2020-12-22 19:58:46 -0300

    Entity Movie, Review, fix Genre

--- backend/src/main/java/br/com/devesuperior/movieflix/entities/Review.java
+++ backend/src/main/java/br/com/devesuperior/movieflix/entities/Review.java
@@ -1,37 +1,33 @@
 package br.com.devesuperior.movieflix.entities;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_genre")
-public class Genre implements Serializable {
+@Table(name = "tb_review")
+public class Review implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
-	private String name;
+	
+	
+	private String text;
+	
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
+	public Review() {}
 
-	@OneToMany(mappedBy = "genre")
-	private Set<Movie> movies = new HashSet<>();
-
-	public Genre() {
-
-	}
-
-	public Genre(Long id, String name) {
+	public Review(Long id, String text) {
+		super();
 		this.id = id;
-		this.name = name;
+		this.text = text;
 	}
 
 	public Long getId() {
@@ -42,16 +38,20 @@ public class Genre implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getText() {
+		return text;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setText(String text) {
+		this.text = text;
 	}
-
-	public Set<Movie> getMovies() {
-		return movies;
+	
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+	
+	public void setMovieId(Long id) {
+		this.movie.setId(id);
 	}
 
 	@Override
@@ -70,7 +70,7 @@ public class Genre implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Genre other = (Genre) obj;
+		Review other = (Review) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -78,5 +78,7 @@ public class Genre implements Serializable {
 			return false;
 		return true;
 	}
-
+	
+	
+	
 }
