467c19fce356de10cf0459d48f06796aeafc48eb
Moisez Dantas <moisez.dantas@visto.global>
2020-12-22 20:01:34 -0300

    DTO add Movie, Review, fix Genre

--- backend/src/main/java/br/com/devesuperior/movieflix/dto/GenreDTO.java
+++ backend/src/main/java/br/com/devesuperior/movieflix/dto/GenreDTO.java
@@ -1,86 +1,64 @@
 package br.com.devesuperior.movieflix.dto;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import br.com.devesuperior.movieflix.entities.Genre;
+import br.com.devesuperior.movieflix.entities.Movie;
 
 public class GenreDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
-	private String title;
-	private String subTitle;
-	private Integer year;
-	private String imgUri;
-	private String synopsis;
+	private String name;
+	private Set<MovieDTO> movies = new HashSet<>();
+	
 	
 	public GenreDTO() {}
-	
-	public GenreDTO(Long id, String title, String subTitle, Integer year, String imgUri, String synopsis) {
-		this.id = id;
-		this.title = title;
-		this.subTitle = subTitle;
-		this.year = year;
-		this.imgUri = imgUri;
-		this.synopsis = synopsis;
-	}
 
+
+	public GenreDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
 	public GenreDTO(Genre entity) {
 		id = entity.getId();
-		title = entity.getTitle();
-		subTitle = entity.getSubTitle();
-		year = entity.getYear();
-		imgUri = entity.getImgUri();
-		synopsis = entity.getSynopsis();
+		name = entity.getName();	
+	}
+	
+	public GenreDTO(Genre entity, Set<Movie> setMovies) {
+		id = entity.getId();
+		name = entity.getName();
+		setMovies.forEach(m -> this.movies.add(new MovieDTO(m)));
 	}
 
 	public Long getId() {
 		return id;
 	}
 
+
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getTitle() {
-		return title;
+
+	public String getName() {
+		return name;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public String getSubTitle() {
-		return subTitle;
-	}
 
-	public void setSubTitle(String subTitle) {
-		this.subTitle = subTitle;
+	public Set<MovieDTO> getMovies() {
+		return movies;
 	}
-
-	public Integer getYear() {
-		return year;
-	}
-
-	public void setYear(Integer year) {
-		this.year = year;
-	}
-
-	public String getImgUri() {
-		return imgUri;
-	}
-
-	public void setImgUri(String imgUri) {
-		this.imgUri = imgUri;
-	}
-
-	public String getSynopsis() {
-		return synopsis;
-	}
-
-	public void setSynopsis(String synopsis) {
-		this.synopsis = synopsis;
-	}
-
+	
+	
 }
